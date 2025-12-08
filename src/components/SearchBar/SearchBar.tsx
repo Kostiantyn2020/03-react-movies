35af20aa5784468989cdfc91c0fe45d4f8e7fff3
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2025-12-08 11:28:42 +0100

    6

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,58 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import toast from "react-hot-toast";
+// import styles from "./SearchBar.module.css";
+
+// interface SearchBarProps {
+//   onSubmit: (query: string) => void;
+// }
+
+// export default function SearchBar({ onSubmit }: SearchBarProps) {
+//   const [query, setQuery] = useState("");
+
+//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     if (!query.trim()) {
+//       toast.error("Please enter your search query.");
+//       return;
+//     }
+
+//     onSubmit(query.trim());
+//     setQuery("");
+//   };
+
+//   return (
+//     <header className={styles.header}>
+//       <div className={styles.container}>
+//         <a
+//           className={styles.link}
+//           href="https://www.themoviedb.org/"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Powered by TMDB
+//         </a>
+
+//         <form className={styles.form} onSubmit={handleSubmit}>
+//           <input
+//             className={styles.input}
+//             type="text"
+//             name="query"
+//             autoComplete="off"
+//             placeholder="Search movies..."
+//             autoFocus
+//             value={query}
+//             onChange={(e) => setQuery(e.target.value)}
+//           />
+//           <button className={styles.button} type="submit">
+//             Search
+//           </button>
+//         </form>
+//       </div>
+//     </header>
+//   );
+// }
+
 import toast from "react-hot-toast";
 import styles from "./SearchBar.module.css";
 
@@ -7,18 +61,21 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const [query, setQuery] = useState("");
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!query.trim()) {
+    const formData = new FormData(e.currentTarget); //** */
+    const query = formData.get("query")?.toString().trim() || ""; //** */
+
+    if (!query) {
+      //** */
       toast.error("Please enter your search query.");
       return;
     }
 
-    onSubmit(query.trim());
-    setQuery("");
+    onSubmit(query); //** */
+
+    e.currentTarget.reset(); // очищення форми
   };
 
   return (
@@ -41,8 +98,6 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
             autoComplete="off"
             placeholder="Search movies..."
             autoFocus
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
           />
           <button className={styles.button} type="submit">
             Search
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,37 +1,48 @@
-import axios from "axios";
+// import axios from "axios";
+// import type { Movie } from "../types/movie";
+
+// const BASE_URL = import.meta.env.VITE_TMDB_API_BASE_URL as string;
+// const TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN as string;
+
+// interface SearchResponse {
+//   page: number;
+//   results: Movie[];
+//   total_pages: number;
+//   total_results: number;
+// }
+
+// if (!BASE_URL) {
+//   console.warn("VITE_TMDB_API_BASE_URL is not set");
+// }
+
+// if (!TOKEN) {
+//   console.warn("VITE_TMDB_ACCESS_TOKEN is not set");
+// }
+
+// export async function fetchMovies(query: string): Promise<SearchResponse> {
+//   const response = await axios.get<SearchResponse>(`${BASE_URL}/search/movie`, {
+//     params: {
+//       query,
+//       include_adult: false,
+//       language: "en-US",
+//       page: 1,
+//     },
+//     headers: {
+//       Authorization: `Bearer ${TOKEN}`,
+//       accept: "application/json",
+//     },
+//   });
+
+//   return response.data;
+// }
+
+import tmdb from "../api/tmdb";
 import type { Movie } from "../types/movie";
 
-const BASE_URL = import.meta.env.VITE_TMDB_API_BASE_URL as string;
-const TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN as string;
-
-interface SearchResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
-
-if (!BASE_URL) {
-  console.warn("VITE_TMDB_API_BASE_URL is not set");
-}
-
-if (!TOKEN) {
-  console.warn("VITE_TMDB_ACCESS_TOKEN is not set");
-}
-
-export async function fetchMovies(query: string): Promise<SearchResponse> {
-  const response = await axios.get<SearchResponse>(`${BASE_URL}/search/movie`, {
-    params: {
-      query,
-      include_adult: false,
-      language: "en-US",
-      page: 1,
-    },
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-      accept: "application/json",
-    },
+export async function searchMovies(query: string): Promise<Movie[]> {
+  const { data } = await tmdb.get("/search/movie", {
+    params: { query },
   });
 
-  return response.data;
+  return data.results || [];
 }
